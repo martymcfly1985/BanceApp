541983cde69f34681ee3012d40dbf12662291e4a
lance hecker <lancehecker@yahoo.com>
2022-01-15 21:43:02 -0500

    Adding in the database project

---
+++ Database/dbo/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[P_Recnum] INT NOT NULL PRIMARY KEY, 
+    [P_FirstName] VARCHAR(50) NULL, 
+    [P_LastName] VARCHAR(50) NULL, 
+    [P_MiddleInitial] VARCHAR(50) NULL, 
+    [P_SSN] NCHAR(10) NULL, 
+    [P_Birthdate] DATE NULL
+)

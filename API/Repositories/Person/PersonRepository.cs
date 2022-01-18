73ce7c71d6773f715c2d3af6a4dad4292e4e25ba
Benjamin Windland <benwindland@gmail.com>
2022-01-17 22:08:52 -0500

    Cleaning up our test code.

--- API/Repositories/Person/PersonRepository.cs
+++ API/Repositories/Person/PersonRepository.cs
@@ -22,8 +22,6 @@ namespace API.Repositories.Person
                     while (reader.Read())
                     {
                         // This is ugly and should be cleaned up but I did various styles of retrieving data to show you the different ways you could do it.
-                        // You are wrong. This is beautiful.
-                        // Lance is a good guy
                         var person = new Models.Person.Person();
                         person.FirstName = reader.IsDBNull(reader.GetOrdinal("P_FirstName")) ? "" : reader.GetString(reader.GetOrdinal("P_FirstName"));
                         person.LastName = reader.IsDBNull(reader.GetOrdinal("P_LastName")) ? "" : reader["P_LastName"].ToString();

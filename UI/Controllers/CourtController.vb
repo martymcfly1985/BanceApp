ee16d42ad31825915204aeb3e2272ae895ed32bb
lance hecker <lancehecker@yahoo.com>
2022-04-12 22:48:32 -0400

    Adding route to backend and trying to hit it from front end

---
+++ UI/Controllers/CourtController.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Web.Http
+Imports API.Services.Tennis
+Imports UI.Plumbing
+Imports HttpGetAttribute = System.Web.Http.HttpGetAttribute
+Imports RouteAttribute = System.Web.Http.RouteAttribute
+
+Namespace Controllers
+    Public Class CourtController
+        Inherits ApiController
+
+        Private ReadOnly Property CourtService As ICourtService
+            Get
+                Return AppContainer.Container.Resolve(Of ICourtService)
+            End Get
+        End Property
+
+        <Route("api/getCourtData")>
+        <HttpGet>
+        Function GetCourts()
+            Dim courts = CourtService.GetCourtInformation()
+        End Function
+    End Class
+End Namespace
--- react-ui/src/BusinessLogic/courtActions.ts
+++ react-ui/src/BusinessLogic/courtActions.ts
@@ -1,5 +1,5 @@
 export const fetchCourtData = async() => {
-  const response = await fetch('getCourtData');
+  const response = await fetch('banceapp/api/getCourtData');
   const data = await response.json();
 
   return data;

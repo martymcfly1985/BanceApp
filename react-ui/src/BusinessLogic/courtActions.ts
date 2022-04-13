c6664c5bbdd893d98dc253a25d43bdbd78848a35
lance hecker <lancehecker@yahoo.com>
2022-04-13 07:41:06 -0400

    connecting front end to backend

--- react-ui/src/BusinessLogic/courtActions.ts
+++ react-ui/src/BusinessLogic/courtActions.ts
@@ -1,5 +1,5 @@
 export const fetchCourtData = async() => {
-  const response = await fetch('banceapp/api/getCourtData');
+  const response = await fetch('api/getCourtData');
   const data = await response.json();
 
   return data;

3e3297e70eaa8ec47c0e25c53f8bd352b52e41ba
lance hecker <lancehecker@yahoo.com>
2022-04-12 21:17:18 -0400

    Add basic logic for switching content on menu click. Add findacourt component

---
+++ react-ui/src/Components/find-a-court/FindACourt.tsx
@@ -0,0 +1,9 @@
+import { Content } from "antd/lib/layout/layout";
+
+const FindACourt = () => (
+  <Content style={{ padding: '50px' }}>
+    <div className="site-layout-content">Find a Court</div>
+  </Content>
+);
+
+export default FindACourt;

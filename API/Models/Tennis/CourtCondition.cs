710a0264f3490b680ac130c33b807fc23fe687d8
Benjamin Windland <benwindland@gmail.com>
2022-09-12 21:24:28 -0400

    Added condition and surface constants

---
+++ API/Models/Tennis/CourtCondition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models.Tennis
+{
+    public class CourtCondition
+    {
+        public const string Excellent = "Excellent";
+        public const string Good = "Good";
+        public const string Fair = "Fair";
+        public const string Poor = "Poor";
+        public const string Disrepair = "Disrepair";
+    }
+}
---
+++ API/Models/Tennis/CourtSurface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models.Tennis
+{
+    public class CourtSurface
+    {
+
+        public const string Clay = "Clay";
+        public const string Grass = "Grass";
+        public const string Hard = "Hard";
+
+    }
+}

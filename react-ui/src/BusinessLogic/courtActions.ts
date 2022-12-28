04c36e3d51348702828cdd41d30b6296824a5e85
lance hecker <lancehecker@yahoo.com>
2022-12-27 20:55:31 -0500

    move and get and post functionality into it's own file and functions

--- react-ui/src/BusinessLogic/courtActions.ts
+++ react-ui/src/BusinessLogic/courtActions.ts
@@ -1,46 +1,15 @@
+import { get, post } from "../CommonFunctions/HttpMethods";
 import { ICourt } from "../Models/Court";
 import { ILocation } from "../Models/Location";
 
 export const fetchLocationData = async() => {
-  const response = await fetch('api/getLocationData');
-  if (!response.ok) {
-    const error = new Error();
-    error.message = response.statusText
-    throw error;
-  }
-  const data: ILocation[] = await response.json();
-
-  return data;
+  return get<ILocation[]>('api/getLocationData');
 }
 
 export const saveNewCourt = async(newCourt: ICourt) => {
-  const response = await fetch('api/submitNewCourt', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(newCourt)
-  });
-  if (!response.ok) {
-    const error = new Error();
-    error.message = response.statusText
-    throw error;
-  }
+  post<any>('api/submitNewCourt', newCourt);
 }
 
 export const saveNewRating = async(rating: number, courtRecnum: number) => {
-  const response = await fetch('api/submitNewRating', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify({rating, courtRecnum})
-  });
-  if (!response.ok) {
-    const error = new Error();
-    error.message = response.statusText
-    throw error;
-  }
-  const updatedCourtCondition: number = await response.json();
-  return updatedCourtCondition;
+  return post<number>('api/submitNewRating', {rating, courtRecnum})
 }

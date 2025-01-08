b1d08c66976d4014064ce766fb97e98bb10cd0f9
Benjamin Windland <benwindland@gmail.com>
2025-01-07 21:28:22 -0500

    Added a spin to indicate loading when sending the verification code to the backend and only turned the input boxes red when the code was incorrect.

--- react-ui/src/Components/sign-in/Verify.tsx
+++ react-ui/src/Components/sign-in/Verify.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Input, message, Row, Space, Typography, } from 'antd';
+import { Button, Card, Col, Input, message, Row, Space, Spin, Typography, } from 'antd';
 import { useNavigate, useParams } from 'react-router-dom';
 import { post } from '../../CommonFunctions/HttpMethods';
 import { VerificationInformation } from '../../Models/User';
@@ -10,6 +10,7 @@ function Verify() {
   const params = useParams();
   const navigate = useNavigate();
   const [verificationLoading, setVerificationLoading] = useState(false);
+  const [incorrectVerificationCode, setIncorrectVerificationCode] = useState(false);
 
   const backSpaceEmptySpaceOrNumber = (input: string) => {
     return /[0-9\\B]/.test(input);
@@ -26,7 +27,8 @@ function Verify() {
     if (isVerificationCodeCorrect) {
       navigate("/signIn"); 
     } else {
-      message.error('Invalid code. Please try again.')
+      message.error('Invalid code. Please try again.');
+      setIncorrectVerificationCode(true);
     }
   }
 
@@ -45,19 +47,22 @@ function Verify() {
           <Space direction="vertical" size={"middle"} style={{alignItems:'center'}}>
             <Title>Welcome to BanceApp!</Title>
             <Title level={3}>Please enter your verification code below.</Title>
-            <Input.OTP 
-              length={6} 
-              onChange={(value) => onVerifyAccountCode(value)}
-              size={"large"}
-              variant={"filled"}
-              status={"error"}
-              disabled={verificationLoading}
-              onKeyDown={(event) => {
-                if (!backSpaceEmptySpaceOrNumber(event.key)) {
-                  event.preventDefault();
-                }
-              }}
-            />
+            <Spin spinning={verificationLoading}>
+              <Input.OTP 
+                length={6} 
+                onChange={(value) => onVerifyAccountCode(value)}
+                size={"large"}
+                
+                variant={"filled"}
+                status={incorrectVerificationCode ? "error" : undefined}
+                disabled={verificationLoading}
+                onKeyDown={(event) => {
+                  if (!backSpaceEmptySpaceOrNumber(event.key)) {
+                    event.preventDefault();
+                  }
+                }}
+              />
+            </Spin>
             <Button type='primary' onClick={() => {message.success('New Verification Code Sent!')}}>Re-Send Code</Button>
           </Space>
         </Card>
